ce761997026c9883df8b29fcd3b41231e153bc54
cburke007 <corey.burke@rackspace.com>
2014-10-29 14:04:24 +0000

    Added FBA Config

---
+++ 6. FBA Config.ps1
@@ -0,0 +1,7 @@
+﻿Add-PSSnapin microsoft.sharepoint.powershell -EA 0
+
+$curloc = Get-Location
+$xmlFile = "Farm-130c2020-99a9-4803-afe6-d5680a3ab323_audit.xml"
+
+[xml]$auditXML = Get-Content "$curloc\$xmlFile"
+
